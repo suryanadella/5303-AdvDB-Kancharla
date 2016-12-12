80d52f3cc8864177fedf88a9ef2de0eaecb0447e
kiran reddy kancharla <kiran.kancharla92@gmail.com>
2016-12-12 00:07:26 -0600

    Create load_yelp.py

---
+++ mongoDB-Project/load_yelp.py
@@ -0,0 +1,21 @@
+import glob
+import ntpath
+import os
+import json
+
+from pymongo import MongoClient
+from bson import Binary, Code
+
+client = MongoClient('localhost', 27017)
+db = client['kiranreddy92']
+collection = db.yelp
+
+
+
+files =  glob.glob("./yelp_challenge_dataset/*.json")
+
+for file in files:
+    f = open(file)
+    for line in f:
+        line = json.loads(line)
+        collection.insert(line)
